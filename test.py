fa5d64428f1b99fbe1cb791e0def6cf66ec974ae
[yashasaxena] <[yasha.saxena@gmail.com]>
2017-11-02 15:20:38 -0400

    modified test.py file, need to jsonify(t)

--- test.py
+++ test.py
@@ -1,34 +1,20 @@
 from flask import Flask, request, jsonify
+import sys
 app = Flask(__name__)
 
+print(sys.path)
 
-# @app.route("/api/heart_rate/summary")
-# def hello():
-#     time = {"time": [0, 1, 2, 3, 4]}
-#     voltage = {"voltage": [30, 40, 50, 60]}
-#     arr = [time, voltage]
-#     return jsonify(arr)
+# @app.route("/api/heart_rate/summary/", methods=['POST'])
+# def trial_get():
+#     print(request.json)
+#     return "done"
 
-@app.route("/api/heart_rate/summary/", methods=['POST'])
+@app.route("/api/heart_rate/summary", methods=['POST'])
 def trial_get():
-    data = request.get_json(force=True, silent=False, cache=True)
+    data = request.json
     t = data['time']
     #t = j_dict['time']
     #v = j_dict['voltage']
 
     #hr = [t, v]
-    return t
-
-# @app.route("/api/heart_rate/summary", methods=['GET'])
-# def trial_get():
-#     j_dict = request.get_json()
-#     t = j_dict['time']
-#     v = j_dict['voltage']
-#
-#     hr = [t, v]
-#     return hr
-#
-#
-# test = trial_get()
-#
-# print(test)
+    return jsonify(t)
