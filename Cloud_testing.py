Created averaging api and finalized pep-8 fixes.

Cloud_testing.py
@@ -24,9 +24,12 @@ def summary():
     """ Runs Web Service
     :param: time: user inputted as json dictionary
     :param: voltage: user inputted as json dictionary
-    :rtype: json dictionary output of time, instantaneous_heart_rate, tachycardia_annotations, brachycardia_annotations
+    :rtype: json dictionary output of time, instantaneous_heart_rate,
+    tachycardia_annotations, brachycardia_annotations
     """
     hr = np.array([])
+    brachy_output = []
+    tachy_output = []
     j_dict = request.json
     try:
         j_dict = json.dumps(j_dict)
@@ -38,64 +41,98 @@ def summary():
     v = np.array(j_dict['voltage'])
     try:
         data_checker = Data(t, v)
-        if data_checker.value_range_result is True & data_checker.data_type_result is True:
+        if data_checker.value_range_result is True \
+                & data_checker.data_type_result is True:
             hr = np.column_stack((t, v))
     except ValueError:
         pass
 
-    # if data_checker.value_range_result is True & data_checker.data_type_result is True:
-    #     hr = np.column_stack((t, v))
-    #
     peak_data = Processing()
     peak_data.ecg_peakdetect(hr)
     peak_times = peak_data.t
-    peak_dict = {"peak": peak_times}
     hr_data = Vitals(peak_times, hr[:, 0])
     inst_hr_array = hr_data.inst_hr_array
     try:
         inst_hr_diagnosis = Diagnosis(inst_hr_array)
-    except ValueError as inst:
-        print(inst.message)
-        send_error(inst.message, 400)
-    brachy_output = inst_hr_diagnosis.brachy_result
-    tachy_output = inst_hr_diagnosis.tachy_result
+        brachy_output = inst_hr_diagnosis.brachy_result
+        tachy_output = inst_hr_diagnosis.tachy_result
+    except ValueError as Inst:
+        print(Inst.args)
+        send_error(Inst.args, 400)
 
     time_dict = {"time": t.tolist()}
     volt_dict = {"voltage": v.tolist()}
     inst_hr_dict = {"instantaneous_heart_rate": inst_hr_array}
     tachy_dict = {"tachycardia_annotations": tachy_output}
     brachy_dict = {"brachycardia_annotations": brachy_output}
-    summary_content = jsonify([time_dict, volt_dict, inst_hr_dict, tachy_dict, brachy_dict])
+    summary_content = jsonify([time_dict, volt_dict, inst_hr_dict,
+                               tachy_dict, brachy_dict])
 
-    summary_test = [time_dict, volt_dict]
-
-
-    #
-    # # inst_hr_dict = {"instantaneous_heart_rate": inst_hr_output.tolist()}
-    # # tachy_dict = {"tachycardia_annotations": tachy_output.tolist()}
-    # # brachy_dict = {"brachycardia_annotations": brachy_output.tolist()}
-    # # summary_content = jsonify[time_dict, inst_hr_dict, tachy_dict, brachy_dict]
-    # hr = hr.tolist()
-    # s = jsonify(hr)
     global counter
     counter = counter + 1
-    #
-    # try:
-    #     json.dumps(summary_content)
-    # except ValueError:
-    #     return send_error("Code corruption, output not successfully converted to JSON", 700)
-    # else:
     return summary_content
-    # json_hr = jsonify(summary_test)
-    # return jsonify(peak_dict)
 
-    #
-    # try:
-    #     json.loads(s)
-    # except ValueError:
-    #     return send_error("Code corruption, output not successfully converted to JSON", 700)
-    # else:
-    #     return s
+
+@app.route("/api/heart_rate/average")
+def average():
+    """ Runs Web Service
+    :param: time: user inputted as json dictionary
+    :param: voltage: user inputted as json dictionary
+    :param: averaging_period: user inputted as json dictionary
+    :rtype: json dictionary output of time_interval,
+    average_heart_rate, tachycardia_annotations, brachycardia_annotations
+    """
+    hr = np.array([])
+    brachy_output = []
+    tachy_output = []
+
+    j_dict = request.json
+    try:
+        j_dict = json.dumps(j_dict)
+        j_dict = json.loads(j_dict)
+        # load is for file, loads is for string
+    except ValueError:
+        return send_error("Input is not JSON dictionary", 600)
+
+    t = np.array(j_dict['time'])
+    v = np.array(j_dict['voltage'])
+    avg_period = np.array(j_dict['averaging_period'])
+
+    try:
+        data_checker = Data(t, v)
+        if data_checker.value_range_result is True \
+                & data_checker.data_type_result is True:
+            hr = np.column_stack((t, v))
+    except ValueError:
+        pass
+
+    peak_data = Processing()
+    peak_data.ecg_peakdetect(hr)
+    peak_times = peak_data.t
+    hr_data = Vitals(peak_times, hr[:, 0])
+    avg_hr_array = hr_data.avg_hr_array
+    try:
+        avg_hr_diagnosis = Diagnosis(avg_hr_array)
+        brachy_output = avg_hr_diagnosis.brachy_result
+        tachy_output = avg_hr_diagnosis.tachy_result
+    except ValueError as Inst:
+        print(Inst.args)
+        send_error(Inst.args, 400)
+
+    avg_period_dict = {"averaging_period": avg_period}
+    time_dict = {"time_interval": t.tolist()}
+    avg_hr_dict = {"average_heart_rate": avg_hr_array}
+    tachy_dict = {"tachycardia_annotations": tachy_output}
+    brachy_dict = {"brachycardia_annotations": brachy_output}
+    average_content = jsonify[avg_period_dict, time_dict,
+                              avg_hr_dict, tachy_dict, brachy_dict]
+    try:
+        json.dumps(average_content)
+    except ValueError:
+        return send_error(
+            "Code corruption, output not successfully converted to JSON", 700)
+    else:
+        return average_content
 
 
 @app.route("/api/requests", methods=['GET'])
@@ -110,52 +147,3 @@ def requests():
     count_json = {"Requests to Date": counter}
     count_json = jsonify(count_json)
     return count_json
-
-
-# @app.route("/api/heart_rate/average")
-# def average():
-#     """ Runs Web Service
-#     :param: time: user inputted as json dictionary
-#     :param: voltage: user inputted as json dictionary
-#     :param: averaging_period: user inputted as json dictionary
-#     :rtype: json dictionary output of time_interval, average_heart_rate, tachycardia_annotations,
-#     brachycardia_annotations
-#     """
-#     j_dict = request.json()
-#     try:
-#         json.loads(j_dict)
-#         # load is for file, loads is for string
-#     except ValueError:
-#         return send_error("Input is not JSON dictionary", 600)
-#     t = np.array(j_dict['time'])
-#     v = np.array(j_dict['voltage'])
-#     avg_period = np.array(j_dict['averaging_period'])
-#     data_checker = Data(t, v)
-#     if data_checker.value_range_result() is True & data_checker.value_type_result() is True:
-#         hr = np.column_stack((t, v))
-#     peak_data = Processing()
-#     peak_data.ecg_peakdetect(hr)
-#     peak_times = peak_data.t
-#     inst_data = Vitals(peak_times)
-#     avg_hr_output = inst_data.avg_hr_array
-#     tachy_output = inst_data.tachy_result
-#     brachy_output = inst_data.brachy_result
-#     avg_period_dict = {"averaging_period": avg_period.tolist()}
-#     time_dict = {"time_interval": t.tolist()}
-#     avg_hr_dict = {"average_heart_rate": avg_hr_output.tolist()}
-#     tachy_dict = {"tachycardia_annotations": tachy_output.tolist()}
-#     brachy_dict = {"brachycardia_annotations": brachy_output.tolist()}
-#     average_content = jsonify[avg_period_dict, time_dict, avg_hr_dict, tachy_dict, brachy_dict]
-#     try:
-#         json.loads(average_content)
-#     except ValueError:
-#         return send_error("Code corruption, output not successfully converted to JSON", 700)
-#     else:
-#         return average_content
-
-# data = Data(t, v)
-# try:
-#     data.verify()
-# except ValueError as inst:
-#     print(inst.message)
-#     send_error(efsdgknf + inst.message, 400)