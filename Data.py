f7f5efbdbd54942eefc731387b14afae5f29b051
Jawad Hoballah <jawad.hoballah@duke.edu>
2017-11-02 23:37:49 -0400

    Fixed indentation that was putting the scope of the methods outside of the Data class.

--- Data.py
+++ Data.py
@@ -15,23 +15,20 @@ class Data:
         self.value_range()
         self.value_type()
 
+    def value_range(self):
+        for x in range(len(self.v)):
+            if x >= 300:
+                print("Your voltage values seem too high!")
+                self.value_range_result = False
+                raise ValueError
+            else:
+                self.value_range_result = True
 
-
-def value_range(self):
-    for x in range(len(self.v)):
-        if x >= 300:
-            print("Your voltage values seem too high!")
-            self.value_range_result = False
-            raise ValueError
+    def value_type(self):
+        for x in range(len(self.v)):
+            if x == str:
+                print("Your data contains strings!")
+                self.data_type_result = False
+                raise ValueError
         else:
-            self.value_range_result = True
-
-
-def value_type(self):
-    for x in range(len(self.v)):
-        if x == str:
-            print("Your data contains strings!")
-            self.data_type_result = False
-            raise ValueError
-    else:
-        self.data_type_result = True
+            self.data_type_result = True
